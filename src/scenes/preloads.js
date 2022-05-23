4b130b9c026875b8a6002cf8fd06cfad36935cb9
valentin-frare <104610033+valentin-frare@users.noreply.github.com>
2022-05-23 15:41:05 -0300

    Update preloads.js

--- src/scenes/preloads.js
+++ src/scenes/preloads.js
@@ -6,14 +6,14 @@ class Preloads extends Phaser.Scene { // Se extiende de Phaser.Scene porque es u
     }
 
     preload() {
-        this.load.image('sad_cow', '../../public/assets/images/sad_cow.png')
-        this.load.image('phaser_logo', '../../public/assets/images/phaser_logo.png')
-        this.load.image('mainmenu_bg', '../../public/assets/images/main_menu_background.png')
-        this.load.image('sky', '../../public/assets/images/sky.png');
-        this.load.image('ground', '../../public/assets/images/platform.png');
-        this.load.image('star', '../../public/assets/images/star.png');
-        this.load.image('bomb', '../../public/assets/images/bomb.png');
-        this.load.spritesheet('dude', '../../public/assets/images/dude.png', { frameWidth: 32, frameHeight: 48 });
+        this.load.image('sad_cow', 'assets/images/sad_cow.png')
+        this.load.image('phaser_logo', 'assets/images/phaser_logo.png')
+        this.load.image('mainmenu_bg', 'assets/images/main_menu_background.png')
+        this.load.image('sky', 'assets/images/sky.png');
+        this.load.image('ground', 'assets/images/platform.png');
+        this.load.image('star', 'assets/images/star.png');
+        this.load.image('bomb', 'assets/images/bomb.png');
+        this.load.spritesheet('dude', 'assets/images/dude.png', { frameWidth: 32, frameHeight: 48 });
     }
 
     create() {
